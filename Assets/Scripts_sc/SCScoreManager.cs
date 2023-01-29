2f4151f82e7f8b54f29e6a3a303201d70522dafe
ywkim1m <ywkim1m@gmail.com>
2023-01-30 00:45:46 +0900

    점수게이지

--- Assets/Scripts_sc/SCScoreManager.cs
+++ Assets/Scripts_sc/SCScoreManager.cs
@@ -11,6 +11,12 @@ public class SCScoreManager : MonoBehaviour
     public int WcurrentScore = 75;
     // 감소 폭 각 스크립트에서 지정
 
+    public Text text;
+
+    int sceneScore;
+
+    bool count = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +26,25 @@ public class SCScoreManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if (Sc1ScObj.scene1)
+        //{
+        //    sceneScore = GameObject.Find("ScObj").GetComponent<Sc1ScObj>().score;
+        //    Sc1ScObj.scene1 = false;
+        //}
+        //else if (Sc2ScObj.scene2)
+        //{
+        //    sceneScore = GameObject.Find("ScObj").GetComponent<Sc2ScObj>().score;
+        //    Sc2ScObj.scene2 = false;
+        //}
+
+        if (count == false)
+        {
+            WcurrentScore += sceneScore;
+            count = true;
+        }
+
         WscoreSlider.value = (float)WcurrentScore / (float)WmaxScore;
+
+        text.text = "뿌듯함 " + sceneScore + "점 상승!" + WcurrentScore;
     }
 }
